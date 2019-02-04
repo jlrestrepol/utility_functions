e20b2299a197956da858c3712e1c1fc13743cbd1
Michal Klein <michal.klein@protonmail.com>
2019-02-04 13:09:42 +0100

    updated corr_ann for multiple components

--- utils.py
+++ utils.py
@@ -433,7 +433,7 @@ def print_numbers(adata, groupby = 'batch'):
           format(adata.n_obs, adata.n_vars))
     
     
-def corr_ann(adata, obs_key = 'n_counts', basis = 'pca', component = 0):
+def corr_ann(adata, obs_keys=['n_counts'], basis='pca', components=[0, 1]):
     """
     Utility function to correlate continious annoations against embedding
     
@@ -444,11 +444,11 @@ def corr_ann(adata, obs_key = 'n_counts', basis = 'pca', component = 0):
     --------
     adata : AnnData object
         Annotatied data matrix
-    obs_key : `str`, optional (default: `"n_counts"`)
+    obs_keys : `list`, optional (default: `["n_counts"]`)
         Key for the continious annotaiton to use
     basis : `str`, optional (default: `"pca"`)
         Key to the basis stored in adata.obsm
-    component : `int`, optional (default: `0`)
+    component : `list`, optional (default: `[1, 2]`)
         Component of the embedding to use
         
     
@@ -459,21 +459,25 @@ def corr_ann(adata, obs_key = 'n_counts', basis = 'pca', component = 0):
     
     # check input
     if 'X_' + basis not in adata.obsm.keys():
-        raise ValueError('You have not computd this basis yet')
-    if obs_key not in adata.obs.keys():
-        raise ValueError('The key \'{}\' does not exist in adata.obs'.format(obs_key))
+        raise ValueError('You have not computed this basis yet')
+    for obs_key in obs_keys:
+        if obs_key not in adata.obs.keys():
+            raise ValueError('The key \'{}\' does not exist in adata.obs'.format(obs_key))
         
-    # get the embedding coordinate
-    X_em = adata.obsm['X_' + basis]
-    X_em = X_em[:, component]
     
     # get the continious annotation
-    ann = adata.obs[obs_key]
-    
-    # compute the correlation coefficient
-    corr = np.corrcoef(X_em, ann)[0, 1]
-    print('Correlation between \'{}\' and component \'{}\' of basis \'{}\' is {:.2f}.'.format(obs_key, 
-        component, basis, corr))
+    for obs_key in obs_keys:
+        ann = adata.obs[obs_key]
+
+        for component in components:
+            # get the embedding coordinate
+            X_em = adata.obsm['X_' + basis]
+            X_em = X_em[:, component]
+        
+            # compute the correlation coefficient
+            corr = np.corrcoef(X_em, ann)[0, 1]
+            print('Correlation between \'{}\' and component \'{}\' of basis \'{}\' is {:.2f}.'.format(obs_key, 
+                component + 1, basis, corr))
 
 
 # quantify the batch effect quickly using the silhouette coefficient
